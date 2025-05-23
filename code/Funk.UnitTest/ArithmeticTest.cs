e0d56633e73e06d911afe0673baefb30d777f8a2
vplauzon <vincent_lauzon@yahoo.com.au>
2025-05-23 15:21:36 -0400

    .

---
+++ code/Funk.UnitTest/ArithmeticTest.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Funk.UnitTest
+{
+    public class ArithmeticTest : BaseTest
+    {
+        [Fact]
+        public void True()
+        {
+            var script = "1 + 2";
+            var primitive = ToPrimitive(script);
+
+            Assert.Equal(3, primitive.ToInteger());
+        }
+    }
+}
