57f52b1403af46d27e8962fd03b410d48561aaff
vplauzon <vincent_lauzon@yahoo.com.au>
2025-05-28 19:20:42 -0400

    .

---
+++ code/Funk.Expression/BinaryArithmeticRule.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Funk.Expression
+{
+    internal class BinaryArithmeticRule : IRule
+    {
+        string IRule.Namespace => "sys";
+
+        string IRule.Name => throw new NotImplementedException();
+
+        ExpressionBase? IRule.Transform(ExpressionBase expression)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
