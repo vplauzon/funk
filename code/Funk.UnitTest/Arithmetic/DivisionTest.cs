da42bbd6606ff2bcf741132621f47f786aec0840
vplauzon <vincent_lauzon@yahoo.com.au>
2025-05-29 18:06:37 -0400

    Add tests

---
+++ code/Funk.UnitTest/Arithmetic/DivisionTest.cs
@@ -0,0 +1,28 @@
+﻿
+namespace Funk.UnitTest.Arithmetic
+{
+    public class DivisionTest : BaseTest
+    {
+        [Fact]
+        public void FloatDivision()
+        {
+            var samples = new[]
+            {
+                (1.5,2.2),
+                (10.26, 25.89),
+                (-500.12, 200.54),
+                (-0.658, 0.54),
+                (-25.982, -12.5)
+            };
+
+            foreach (var sample in samples)
+            {
+                var script = $"{sample.Item1} / {sample.Item2}";
+                var expectedValue = sample.Item1 / sample.Item2;
+                var primitive = ToFloat(script);
+
+                Assert.Equal(expectedValue, primitive);
+            }
+        }
+    }
+}
