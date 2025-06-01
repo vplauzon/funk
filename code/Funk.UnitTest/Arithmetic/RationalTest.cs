189b7c565d19d011b514142239db7195734dceb4
vplauzon <vincent_lauzon@yahoo.com.au>
2025-06-01 16:44:59 -0400

    Refactor tests

--- code/Funk.UnitTest/Arithmetic/RationalTest.cs
+++ code/Funk.UnitTest/Arithmetic/RationalTest.cs
@@ -5,95 +5,59 @@ namespace Funk.UnitTest.Arithmetic
 
     public class RationalTest : BaseTest
     {
-        [Fact]
-        public void RationalToFloat()
+        [Theory]
+        [InlineData(1, 2, 0.5f)]
+        [InlineData(10, 25, 0.4f)]
+        [InlineData(-500, 200, -2.5f)]
+        [InlineData(-25, -12, 2.083333f)]
+        public void RationalToFloat(int numerator, int denominator, float expected)
         {
-            var samples = new[]
-            {
-                (1,2),
-                (10, 25),
-                (-500, 200),
-                (-25, -12)
-            };
-
-            foreach (var sample in samples)
-            {
-                var script = $"toFloat({sample.Item1}/{sample.Item2})";
-                var expectedValue = (float)sample.Item1 / sample.Item2;
-                var primitive = ToFloat(script);
-
-                Assert.True(Math.Abs(expectedValue - primitive) < 0.000001);
-            }
+            var script = $"toFloat({numerator}/{denominator})";
+            var primitive = ToFloat(script);
+            Assert.True(Math.Abs(expected - primitive) < 0.000001);
         }
 
-        [Fact]
-        public void RationalToInteger()
+        [Theory]
+        [InlineData(2, 1, 2)]
+        [InlineData(4, 2, 2)]
+        [InlineData(6, 2, 3)]
+        [InlineData(9, 3, 3)]
+        [InlineData(-15, 3, -5)]
+        [InlineData(-15, -5, 3)]
+        public void RationalToInteger(int numerator, int denominator, int expected)
         {
-            var samples = new[]
-            {
-                (2,1),
-                (4,2),
-                (6,2),
-                (9,3),
-                (-15,3),
-                (-15,-5)
-            };
-
-            foreach (var sample in samples)
-            {
-                var script = $"{sample.Item1}/{sample.Item2}";
-                var expectedValue = sample.Item1 / sample.Item2;
-                var primitive = ToInteger(script);
-
-                Assert.Equal(expectedValue, primitive);
-            }
+            var script = $"{numerator}/{denominator}";
+            var primitive = ToInteger(script);
+            Assert.Equal(expected, primitive);
         }
 
-        [Fact]
-        public void SumToRational()
+        [Theory]
+        [InlineData(1, 2, 1, 4, 3, 4)]  // 1/2 + 1/4 = 3/4
+        [InlineData(-1, 8, 1, 4, 1, 8)] // -1/8 + 1/4 = 1/8
+        [InlineData(1, 8, 1, 8, 1, 4)]  // 1/8 + 1/8 = 1/4
+        public void SumToRational(int n1, int d1, int n2, int d2, int expectedNum, int expectedDen)
         {
-            var samples = new[]
-            {
-                ((1,2),(1,4),(3,4)),
-                ((-1,8),(1,4),(1,8)),
-                ((1,8),(1,8),(1,4)),
-            };
+            var sumScript = $"{n1}/{d1}+{n2}/{d2}";
+            var numeratorScript = $"param({sumScript}, 0)";
+            var denumeratorScript = $"param({sumScript}, 1)";
+            
+            var numerator = ToInteger(numeratorScript);
+            var denumerator = ToInteger(denumeratorScript);
 
-            foreach (var sample in samples)
-            {
-                var sumScript = $"{sample.Item1.Item1}/{sample.Item1.Item2}" +
-                    $"+{sample.Item2.Item1}/{sample.Item2.Item2}";
-                var numeratorScript = $"param({sumScript}, 0)";
-                var denumeratorScript = $"param({sumScript}, 1)";
-                var numerator = ToInteger(numeratorScript);
-                var denumerator = ToInteger(denumeratorScript);
-
-                Assert.Equal(sample.Item3.Item1, numerator);
-                Assert.Equal(sample.Item3.Item2, denumerator);
-            }
+            Assert.Equal(expectedNum, numerator);
+            Assert.Equal(expectedDen, denumerator);
         }
 
-        [Fact]
-        public void SumToInteger()
+        [Theory]
+        [InlineData(34, 35, 1, 35, 1)]  // 34/35 + 1/35 = 1
+        [InlineData(1, 2, 1, 2, 1)]     // 1/2 + 1/2 = 1
+        [InlineData(1, 4, 3, 4, 1)]     // 1/4 + 3/4 = 1
+        [InlineData(-1, 4, 2, 8, 0)]    // -1/4 + 2/8 = 0
+        public void SumToInteger(int n1, int d1, int n2, int d2, int expected)
         {
-            var samples = new[]
-            {
-                (34,35,1,35),
-                (1,2,1,2),
-                (1,4,3,4),
-                (-1,4,2,8)
-            };
-
-            foreach (var sample in samples)
-            {
-                var script = $"{sample.Item1}/{sample.Item2}+{sample.Item3}/{sample.Item4}";
-                var expectedValue =
-                    ((sample.Item1 * sample.Item4) + (sample.Item3 * sample.Item2))
-                    / (sample.Item2 * sample.Item4);
-                var primitive = ToInteger(script);
-
-                Assert.Equal(expectedValue, primitive);
-            }
+            var script = $"{n1}/{d1}+{n2}/{d2}";
+            var primitive = ToInteger(script);
+            Assert.Equal(expected, primitive);
         }
     }
 }
