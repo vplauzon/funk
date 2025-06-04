36543f00f9fb469004dafc12693be0062b50f57f
vplauzon <vincent_lauzon@yahoo.com.au>
2025-06-04 19:10:15 -0400

    Add failing test

---
+++ code/Funk.UnitTest/IfTest.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Funk.UnitTest
+{
+    public class IfTest : BaseTest
+    {
+        [Theory]
+        [InlineData(true, 1)]
+        [InlineData(false, 2)]
+        public void IfBranchSelection(bool condition, int expected)
+        {
+            var script = $"if({condition.ToString().ToLower()}, 1, 2)";
+            var primitive = ToInteger(script);
+
+            Assert.Equal(expected, primitive);
+        }
+    }
+}
