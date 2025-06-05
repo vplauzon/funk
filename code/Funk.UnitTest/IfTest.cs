ddf359c337bf3c5ff4136d0530b2e01dfef8f8c3
vplauzon <vincent_lauzon@yahoo.com.au>
2025-06-04 23:38:44 -0400

    .

--- code/Funk.UnitTest/IfTest.cs
+++ code/Funk.UnitTest/IfTest.cs
@@ -6,9 +6,49 @@ namespace Funk.UnitTest
         [Theory]
         [InlineData(true, 1)]
         [InlineData(false, 2)]
-        public void IfBranchSelection(bool condition, int expected)
+        public void TernaryIf(bool condition, int expected)
         {
             var script = $"if({condition.ToString().ToLower()}, 1, 2)";
+            var primitive = ToInteger(script);
+
+            Assert.Equal(expected, primitive);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        [InlineData(5)]
+        [InlineData(6)]
+        public void ChainedIfElse(int input)
+        {
+            var script = @$"if({input}==1)
+{{
+    return 2;
+}}
+else if({input}==2)
+{{
+return 3;
+}}
+else if({input}==3)
+{{
+return 4;
+}}
+else if({input}==4)
+{{
+return 5;
+}}
+else if({input}==5)
+{{
+return 6;
+}}
+else
+{{
+return 7;
+}}
+";
+            var expected = input + 1;
             var primitive = ToInteger(script);
 
             Assert.Equal(expected, primitive);
