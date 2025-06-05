13b904fecf194ec7c75b1d3d78148a662c7d4f85
vplauzon <vincent_lauzon@yahoo.com.au>
2025-06-04 23:57:24 -0400

    .

--- code/Funk.UnitTest/Logic/InequalityTest.cs
+++ code/Funk.UnitTest/Logic/InequalityTest.cs
@@ -1,58 +1,7 @@
 ﻿namespace Funk.UnitTest.Logic
 {
-    public class IfTest : BaseTest
+    public class InequalityTest : BaseTest
     {
-        [Theory]
-        [InlineData(true, 1)]
-        [InlineData(false, 2)]
-        public void TernaryIf(bool condition, int expected)
-        {
-            var script = $"if({condition.ToString().ToLower()}, 1, 2)";
-            var primitive = ToInteger(script);
-
-            Assert.Equal(expected, primitive);
-        }
-
-        [Theory]
-        [InlineData(1)]
-        [InlineData(2)]
-        [InlineData(3)]
-        [InlineData(4)]
-        [InlineData(5)]
-        [InlineData(6)]
-        public void ChainedIfElse(int input)
-        {
-            var script = @$"if({input}==1)
-{{
-    return 2;
-}}
-else if({input}==2)
-{{
-return 3;
-}}
-else if({input}==3)
-{{
-return 4;
-}}
-else if({input}==4)
-{{
-return 5;
-}}
-else if({input}==5)
-{{
-return 6;
-}}
-else
-{{
-return 7;
-}}
-";
-            var expected = input + 1;
-            var primitive = ToInteger(script);
-
-            Assert.Equal(expected, primitive);
-        }
-
         [Theory]
         [InlineData(1, 1)]
         [InlineData(1, 2)]
@@ -77,6 +26,84 @@ return 7;
             var primitive = ToBoolean(script);
 
             Assert.Equal(a <= b, primitive);
+        }
+
+        [Theory]
+        [InlineData(1, 1)]
+        [InlineData(1, 2)]
+        [InlineData(2, 1)]
+        [InlineData(2, 2)]
+        public void GreaterThanInteger(int a, int b)
+        {
+            var script = $"{a}>{b}";
+            var primitive = ToBoolean(script);
+
+            Assert.Equal(a > b, primitive);
+        }
+
+        [Theory]
+        [InlineData(1, 1)]
+        [InlineData(1, 2)]
+        [InlineData(2, 1)]
+        [InlineData(2, 2)]
+        public void GreaterOrEqualToInteger(int a, int b)
+        {
+            var script = $"{a}>={b}";
+            var primitive = ToBoolean(script);
+
+            Assert.Equal(a >= b, primitive);
+        }
+
+        [Theory]
+        [InlineData(1.0, 1.0)]
+        [InlineData(1.0, 2.0)]
+        [InlineData(2.0, 1.0)]
+        [InlineData(2.0, 2.0)]
+        public void LessThanDouble(double a, double b)
+        {
+            var script = $"{a}<{b}";
+            var primitive = ToBoolean(script);
+
+            Assert.Equal(a < b, primitive);
+        }
+
+        [Theory]
+        [InlineData(1.0, 1.0)]
+        [InlineData(1.0, 2.0)]
+        [InlineData(2.0, 1.0)]
+        [InlineData(2.0, 2.0)]
+        public void LessOrEqualToDouble(double a, double b)
+        {
+            var script = $"{a}<={b}";
+            var primitive = ToBoolean(script);
+
+            Assert.Equal(a <= b, primitive);
+        }
+
+        [Theory]
+        [InlineData(1.0, 1.0)]
+        [InlineData(1.0, 2.0)]
+        [InlineData(2.0, 1.0)]
+        [InlineData(2.0, 2.0)]
+        public void GreaterThanDouble(double a, double b)
+        {
+            var script = $"{a}>{b}";
+            var primitive = ToBoolean(script);
+
+            Assert.Equal(a > b, primitive);
+        }
+
+        [Theory]
+        [InlineData(1.0, 1.0)]
+        [InlineData(1.0, 2.0)]
+        [InlineData(2.0, 1.0)]
+        [InlineData(2.0, 2.0)]
+        public void GreaterOrEqualToDouble(double a, double b)
+        {
+            var script = $"{a}>={b}";
+            var primitive = ToBoolean(script);
+
+            Assert.Equal(a >= b, primitive);
         }
     }
 }
